00e277f0c5ac656b6257eaf35e3a2ec506a1a6eb
Pedro Tavares de Carvalho <pcarvalho@cyral.com>
2021-07-18 14:20:59 -0300

    Adds documentation and helper methods for logger class

--- src/logs.cpp
+++ src/logs.cpp
@@ -2,9 +2,14 @@
 
 #include <iostream>
 
-namespace logger {
-Logger::Logger() {
-    if (getenv("VERBOSITY") != NULL) verbosity = atoi(getenv("VERBOSITY"));
-}
-}  // namespace logger
+void logger::Logger::SetLogLevel(logger::LogLevel verbosity) {
+    this->verbosity = verbosity;
+};
+logger::LogLevel logger::Logger::GetLogLevel() { return verbosity; }
 
+logger::Logger::Logger(std::ostream& _out) : out(_out) {
+    if (getenv("VERBOSITY") != NULL)
+	verbosity = LogLevel(atoi(getenv("VERBOSITY")));
+}
+logger::Logger::Logger(logger::LogLevel _verbosity, std::ostream& _out)
+    : verbosity(_verbosity), out(_out) {}

a2632b9024b7bc64b1e8b54d10bc6cdf2c483896
Francisco Andrade <francisco@inspr.com>
2021-07-19 21:52:32 -0300

    Updated main to generate .mv file

--- src/main.cpp
+++ src/main.cpp
@@ -11,20 +11,27 @@
 
 #include "logs.h"
 
-bool is_number(const std::string& s) {
+bool is_number(const std::string &s)
+{
   std::string::const_iterator it = s.begin();
-  while (it != s.end() && std::isdigit(*it)) ++it;
+  while (it != s.end() && std::isdigit(*it))
+    ++it;
   return !s.empty() && it == s.end();
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv)
+{
   logger::Logger logger = logger::Logger();
-  if (argc < 2) {
+  if (argc < 2)
+  {
     logger.ERROR("usage: montador <file>");
   }
-  std::ifstream t = std::ifstream(argv[1]);
-  if (!t.good()) {
-    logger.ERROR(std::string("invalid file name: ") + std::string(argv[1]));
+
+  std::string inputFile = std::string(argv[1]);
+  std::ifstream t = std::ifstream(inputFile);
+  if (!t.good())
+  {
+    logger.ERROR(std::string("invalid file name: ") + inputFile);
   }
   std::stringstream buffer;
   buffer << t.rdbuf();
@@ -38,39 +45,55 @@ int main(int argc, char** argv) {
       {"R3", 3},
   };
   std::map<std::string, std::pair<int, std::vector<std::string>>> operations = {
-      {"HALT", {0, {}}},          {"LOAD", {1, {"R", "M"}}},
-      {"STORE", {2, {"R", "M"}}}, {"READ", {3, {"R"}}},
-      {"WRITE", {4, {"R"}}},      {"COPY", {5, {"R", "R"}}},
-      {"PUSH", {6, {"R"}}},       {"POP", {7, {"R"}}},
-      {"ADD", {8, {"R", "R"}}},   {"SUB", {9, {"R", "R"}}},
-      {"MUL", {10, {"R", "R"}}},  {"DIV", {11, {"R", "R"}}},
-      {"MOD", {12, {"R", "R"}}},  {"AND", {13, {"R", "R"}}},
-      {"OR", {14, {"R", "R"}}},   {"NOT", {15, {"M"}}},
-      {"JUMP", {16, {"M"}}},      {"JZ", {17, {"M"}}},
-      {"JN", {18, {"M"}}},        {"CALL", {19, {"M"}}},
-      {"RET", {20, {}}},          {"WORD", {0, {"M"}}},
+      {"HALT", {0, {}}},
+      {"LOAD", {1, {"R", "M"}}},
+      {"STORE", {2, {"R", "M"}}},
+      {"READ", {3, {"R"}}},
+      {"WRITE", {4, {"R"}}},
+      {"COPY", {5, {"R", "R"}}},
+      {"PUSH", {6, {"R"}}},
+      {"POP", {7, {"R"}}},
+      {"ADD", {8, {"R", "R"}}},
+      {"SUB", {9, {"R", "R"}}},
+      {"MUL", {10, {"R", "R"}}},
+      {"DIV", {11, {"R", "R"}}},
+      {"MOD", {12, {"R", "R"}}},
+      {"AND", {13, {"R", "R"}}},
+      {"OR", {14, {"R", "R"}}},
+      {"NOT", {15, {"M"}}},
+      {"JUMP", {16, {"M"}}},
+      {"JZ", {17, {"M"}}},
+      {"JN", {18, {"M"}}},
+      {"CALL", {19, {"M"}}},
+      {"RET", {20, {}}},
+      {"WORD", {0, {"M"}}},
   };
   std::vector<std::string> first_pass = std::vector<std::string>();
   logger.INFO("initiating first pass");
-  for (std::string line; getline(buffer, line);) {
+  for (std::string line; getline(buffer, line);)
+  {
     logger.DEBUG("line:", line);
     std::stringstream buf;
     buf.str(line);
     buf >> op;
-    if (op[0] == ';') {
+    if (op[0] == ';')
+    {
       logger.DEBUG("interpreting line as comment");
       continue;
     }
 
-    if (op == "END") break;
-    if (op.back() == ':') {
+    if (op == "END")
+      break;
+    if (op.back() == ':')
+    {
       registers[op.substr(0, op.size() - 1)] = fp;
       logger.DEBUG("label:", op.substr(0, op.size() - 1));
       buf >> op;
     }
     logger.DEBUG("operation:", op);
     first_pass.push_back(op);
-    for (int i = 0; i < operations[op].second.size(); ++i) {
+    for (int i = 0; i < operations[op].second.size(); ++i)
+    {
       std::string r;
       buf >> r;
       logger.DEBUG("parameter:", r);
@@ -82,9 +105,11 @@ int main(int argc, char** argv) {
   logger.INFO("initiating second pass");
   std::stringstream compiled;
   int i = 0;
-  for (; i < first_pass.size(); ++i) {
+  for (; i < first_pass.size(); ++i)
+  {
     std::string op = first_pass[i];
-    if (op == "WORD") {
+    if (op == "WORD")
+    {
       logger.DEBUG("WORD:",
                    "treating as word, printing value: " + first_pass[i + 1]);
       compiled << first_pass[++i] << " ";
@@ -92,25 +117,32 @@ int main(int argc, char** argv) {
     }
 
     logger.DEBUG("operation:", op);
-    logger.DEBUG("translating opeartion", operations[op].first);
+    logger.DEBUG("translating operation", operations[op].first);
     compiled << operations[op].first << " ";
-    for (std::string& reg : operations[op].second) {
+    for (std::string &reg : operations[op].second)
+    {
       i++;
       std::string e = i == first_pass.size() - 1 ? "" : " ";
-      if (reg == "R") {
+      if (reg == "R")
+      {
         logger.DEBUG("treating input as register:", first_pass[i]);
         logger.DEBUG("translating input to: ", registers[first_pass[i]]);
         if (registers.find(first_pass[i]) == registers.end())
           logger.ERROR("unable to find identifier", first_pass[i]);
         compiled << registers[first_pass[i]] << e;
-      } else if (reg == "M") {
+      }
+      else if (reg == "M")
+      {
         logger.DEBUG("treating input as number");
-        if (registers.find(first_pass[i]) == registers.end()) {
+        if (registers.find(first_pass[i]) == registers.end())
+        {
           if (!is_number(first_pass[i]))
             logger.ERROR("unknown identifier:", first_pass[i]);
           logger.DEBUG("printing input directly:", first_pass[i]);
           compiled << first_pass[i] << e;
-        } else {
+        }
+        else
+        {
           logger.DEBUG("identifier", first_pass[i], "found, printing",
                        registers[first_pass[i]]);
           compiled << (registers[first_pass[i]] - i - 1) << e;
@@ -118,11 +150,19 @@ int main(int argc, char** argv) {
       }
     }
   }
-  logger.INFO("compilation complete, outputing to stdout");
-  logger.DEBUG("printing header");
-  std::cout << "MV-EXE" << std::endl;
-  logger.DEBUG("printing program information.", "size:", i - 1);
-  std::cout << (i - 1) << " 100 999 100" << std::endl;
-  logger.DEBUG("printing compiled program");
-  std::cout << compiled.rdbuf() << std::endl;
+  logger.INFO("compilation complete, generation .mv file");
+  std::string outputFile = inputFile.substr(0, inputFile.find(".")) + ".mv";
+  std::ofstream outfile(outputFile);
+
+  outfile << "MV-EXE\n"
+          << std::endl;
+
+  outfile << (i - 1) << " 100 999 100\n"
+          << std::endl;
+
+  outfile << compiled.rdbuf() << std::endl;
+
+  logger.DEBUG(outputFile + " generated");
+
+  outfile.close();
 }

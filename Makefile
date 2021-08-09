6b0d4239ad7242ca823c4cf835715c1413f6adce
Pedro Tavares de Carvalho <pcarvalho@cyral.com>
2021-08-09 17:43:57 -0300

    Fixes WORD relative function pointer

--- Makefile
+++ Makefile
@@ -5,3 +5,8 @@ build:
 	mkdir -p bin
 	cp ligador/bin/ligador bin
 	cp montador/bin/montador bin
+
+test: build
+	./bin/montador ligador/tst/ex_pt1.txt > ligador/tst/ex_pt1.mv
+	./bin/montador ligador/tst/ex_pt2.txt > ligador/tst/ex_pt2.mv
+	./bin/ligador ligador/tst/ex_pt1.mv ligador/tst/ex_pt2.mv | tee ligador/tst/ex_final.mv
--- ligador/src/main.cpp
+++ ligador/src/main.cpp
@@ -11,54 +11,36 @@
 
 #include "logs.h"
 
-bool is_number(const std::string &s)
-{
+bool is_number(const std::string &s) {
   std::string::const_iterator it = s.begin();
-  if (*it == '-')
-    ++it;
-  while (it != s.end() && std::isdigit(*it))
-    ++it;
+  if (*it == '-') ++it;
+  while (it != s.end() && std::isdigit(*it)) ++it;
   return !s.empty() && it == s.end();
 }
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
   logger::Logger logger = logger::Logger();
-  std::map<std::string, int> registers = {};
+  std::map<std::string, int> registers = {
+      {"R0", 0},
+      {"R1", 1},
+      {"R2", 2},
+      {"R3", 3},
+  };
   int cur = 0;
   std::vector<std::string> compiled = std::vector<std::string>();
   int entry;
   std::vector<std::vector<std::string>> operations = {
-      {},
-      {"R", "M"},
-      {"R", "M"},
-      {"R"},
-      {"R"},
-      {"R", "R"},
-      {"R"},
-      {"R"},
-      {"R", "R"},
-      {"R", "R"},
-      {"R", "R"},
-      {"R", "R"},
-      {"R", "R"},
-      {"R", "R"},
-      {"R", "R"},
-      {"M"},
-      {"M"},
-      {"M"},
-      {"M"},
-      {"M"},
-      {},
-      {"R"},
+      {},         {"R", "M"}, {"R", "M"}, {"R"},      {"R"},      {"R", "R"},
+      {"R"},      {"R"},      {"R", "R"}, {"R", "R"}, {"R", "R"}, {"R", "R"},
+      {"R", "R"}, {"R", "R"}, {"R", "R"}, {"M"},      {"M"},      {"M"},
+      {"M"},      {"M"},      {},         {"R"},
   };
 
-  for (int i = 1; i < argc; ++i)
-  {
+  for (int i = 1; i < argc; ++i) {
+    int size;
     std::string inputFile = std::string(argv[i]);
     std::ifstream t = std::ifstream(inputFile);
-    if (!t.good())
-    {
+    if (!t.good()) {
       logger.ERROR(std::string("invalid file name: ") + inputFile);
     }
     std::stringstream buffer;
@@ -68,12 +50,19 @@ int main(int argc, char **argv)
     getline(buffer, line);
     std::stringstream buf;
     buf.str(line);
+
+    buf >> size;
+
     std::string label;
-    do
-    {
+
+    getline(buffer, line);
+
+    buf = std::stringstream();
+    buf.str(line);
+    do {
       buf >> label;
-      if (buf.eof())
-        break;
+      // if (buf.eof())
+      // break;
       int value;
       buf >> value;
       registers[label] = value + cur;
@@ -84,15 +73,14 @@ int main(int argc, char **argv)
 
     buf = std::stringstream();
     buf.str(line);
-    do
-    {
+    do {
       buf >> label;
       // if (buf.eof())
-      //   break;
-      cur++;
+      // break;
       compiled.push_back(label);
       logger.INFO("reading value:", label);
     } while (!buf.eof());
+    cur += size;
   }
 
   std::cout << "MV-EXE" << std::endl;
@@ -103,28 +91,26 @@ int main(int argc, char **argv)
 
   int instr;
   int currVal;
-  for (int i = 0; i < compiled.size(); ++i)
-  {
+  int fp = 0;
+  for (int i = 0; i < compiled.size(); ++i) {
     auto k = compiled[i];
     instr = atoi(k.c_str());
-    if (instr != 21)
-    {
+    if (instr != 21) {
       std::cout << instr << " ";
+      fp++;
     }
-    for (auto j : operations[instr])
-    {
+    for (auto j : operations[instr]) {
       k = compiled[++i];
+      fp++;
       if (is_number(k))
         currVal = atoi(k.c_str());
       else if (registers.find(k) == registers.end())
         logger.ERROR("unable to find label", k);
-      else
-      {
+      else {
         currVal = registers[k];
         logger.INFO("currval:", currVal);
-        if (j == "M")
-        {
-          currVal -= i;
+        if (j == "M") {
+          currVal -= fp;
         }
         logger.INFO("i:", i);
         logger.INFO("k:", k);
